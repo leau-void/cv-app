6222b55dadbd94af85b74f1274020f782e4af746
leau-void <lpierra@gmail.com>
2021-10-09 10:56:37 -0300

    refac outerform

--- src/components/OuterForm.js
+++ src/components/OuterForm.js
@@ -1,33 +1,29 @@
 import "../styles/outer-form.css";
 import "../styles/zone.css";
 import "../styles/button.css";
-import React, { Component } from "react";
+import React from "react";
 import GeneralInfo from "./GeneralInfo";
 import EducationInfo from "./EducationInfo";
 import ProfessionalInfo from "./ProfessionalInfo";
 import Skills from "./Skills";
 
-class OuterForm extends Component {
-  render() {
-    const { state, handlers } = this.props;
-    return (
-      <div className="outer-form">
-        <GeneralInfo state={state.general} handlers={handlers} />
-        <ProfessionalInfo stateArray={state.professional} handlers={handlers} />
-        <EducationInfo stateArray={state.education} handlers={handlers} />
-        <Skills state={state.skills} handlers={handlers} />
+const OuterForm = (props) => {
+  const { state, handlers } = props;
+  return (
+    <div className="outer-form">
+      <GeneralInfo {...{ state: state.general, handlers }} />
+      <ProfessionalInfo {...{ stateArray: state.professional, handlers }} />
+      <EducationInfo {...{ stateArray: state.education, handlers }} />
+      <Skills {...{ state: state.skills, handlers }} />
 
-        <button
-          className="button button_example"
-          onClick={handlers.loadExample}>
-          Load Example
-        </button>
-        <button className="button button_reset" onClick={handlers.resetForm}>
-          Reset
-        </button>
-      </div>
-    );
-  }
-}
+      <button className="button button_example" onClick={handlers.loadExample}>
+        Load Example
+      </button>
+      <button className="button button_reset" onClick={handlers.resetForm}>
+        Reset
+      </button>
+    </div>
+  );
+};
 
 export default OuterForm;
